d121420817a5eb3242c2fe5a9c3c3490b585e7ca
wanirepo <clinpsywoo@gmail.com>
2017-04-10 01:51:32 +0900

    removed an old function

--- Code/experiment_path_def.m
+++
@@ -1,8 +0,0 @@
-basedir = '/Users/cnir/Documents/cocoanlab/animal_fMRI';
-analysis_path = fullfile(basedir, 'code');
-toolbox_path = fullfile(basedir, 'toolboxes');
-atlas_path = fullfile(basedir, 'Wisconsin_atlas');
-
-addpath(genpath(analysis_path));
-addpath(genpath(toolbox_path));
-addpath(genpath(atlas_path));
